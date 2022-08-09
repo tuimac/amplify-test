8280ed6a4cbeed503f1ef34fae95c46ec766819d
tuimac <tuimac.devadm01@gmail.com>
2022-08-09 22:43:21 +0900

    save

--- amplify/backend/function/tasks/src/index.js
+++ amplify/backend/function/tasks/src/index.js
@@ -1,15 +1,32 @@
+const mysql = require('mysql');
 
+const connection = mysql.createConnection({
+  host: 'database-1.cjsttbydw6j6.ap-northeast-1.rds.amazonaws.com',
+  user: 'admin',
+  password: 'password',
+  database: 'todomanager'
+});
 
-/**
- * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
- */
-exports.handler = async (event) => {
-    return {
-      statusCode: 200,
-      headers: {
-          "Access-Control-Allow-Origin": "*",
-          "Access-Control-Allow-Headers": "*"
-      }, 
-      body: event
-    };
+const response = {
+  statusCode: 200,
+  headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Headers": "*"
+  }, 
+  body: ""
+};
+
+exports.handler = (event, context, callback) => {
+  const sql = 'select * from TODOS;';
+  var res = '';
+
+  connection.query(sql, function(err, result) {
+    if (err) {
+      console.log(err);
+      return err;
+    }
+    response['body'] = JSON.stringify(result)
+    callback(null, response);
+  });
+  connection.end();
 };
