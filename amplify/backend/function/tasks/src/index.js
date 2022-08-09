871df6daa12737d6184f445b7e09048325ec6b76
tuimac <tuimac.devadm01@gmail.com>
2022-08-09 21:53:16 +0900

    save

--- amplify/backend/function/tasks/src/index.js
+++ amplify/backend/function/tasks/src/index.js
@@ -3,7 +3,7 @@
 /**
  * @type {import('@types/aws-lambda').APIGatewayProxyHandler}
  */
-exports.handler = async (event) => {
+exports.handler = (event) => {
     return {
       statusCode: 200,
       headers: {
